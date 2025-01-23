20d3222aa8a10829652d944fe55a7a6076971ebc
김병웅 <quddnddl35@naver.com>
2025-01-23 20:19:42 +0900

    Feat : 장소 구독 구현

--- src/main/java/personal_projects/backend/domain/bookmark/domain/BookMark.java
+++ src/main/java/personal_projects/backend/domain/bookmark/domain/BookMark.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import personal_projects.backend.domain.place.domain.Place;
 import personal_projects.backend.domain.user.domain.User;
 
+import java.awt.print.Book;
+
 @Entity
 @Builder
 @Getter
@@ -25,4 +27,11 @@ public class BookMark {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public static BookMark of(Place place, User user) {
+        return BookMark.builder()
+            .place(place)
+            .user(user)
+            .build();
+    }
 }
--- src/main/java/personal_projects/backend/domain/bookmark/service/BookMarkService.java
+++ src/main/java/personal_projects/backend/domain/bookmark/service/BookMarkService.java
@@ -2,18 +2,41 @@ package personal_projects.backend.domain.bookmark.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import personal_projects.backend.domain.bookmark.domain.BookMark;
 import personal_projects.backend.domain.bookmark.exception.BookMarkNotFoundException;
 import personal_projects.backend.domain.bookmark.exception.errorCode.BookMarkErrorCode;
 import personal_projects.backend.domain.bookmark.repository.BookMarkRepository;
+import personal_projects.backend.domain.place.domain.Place;
+import personal_projects.backend.domain.place.service.PlaceService;
+import personal_projects.backend.domain.user.domain.User;
+import personal_projects.backend.domain.user.service.UserService;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BookMarkService {
 
     private final BookMarkRepository bookMarkRepository;
+    private final UserService userService;
+    private final PlaceService placeService;
 
     public BookMark findById(Long id) {
         return bookMarkRepository.findById(id).orElseThrow(() -> new BookMarkNotFoundException(BookMarkErrorCode.BOOKMARK_NOT_FOUND));
+    }
+
+    @Transactional
+    public void checkBookMark(Long placeId, Long userId) {
+        User user = userService.findById(userId);
+        Place place = placeService.findById(placeId);
+
+        bookMarkRepository.findByPlaceAndUser(place, user).ifPresentOrElse(
+                bookMark -> {
+                    bookMarkRepository.delete(bookMark);
+                },
+                () -> {
+                    bookMarkRepository.save(BookMark.of(place, user));
+                }
+        );
     }
 }
